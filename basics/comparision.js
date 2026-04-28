063b5f9e95b59ff0491a053da393c29bb5266253
MrinmoyPatra-Pravaah <mrinmoy@pravaahc.com>
2026-04-28 07:57:20 +0000

    Comparision

---
+++ basics/comparision.js
@@ -0,0 +1,51 @@
+console.log(2>1); // true
+console.log(2<1); // false
+console.log(2==1); // false
+console.log(2!=1); // true
+
+console.log(2>=1); // true
+console.log(2<=1); // false
+
+console.log("abc" > "abd"); // false
+console.log("abc" < "abd"); // true
+console.log("abc" == "abc"); // true
+console.log("abc" != "abc"); // false
+
+console.log(true > false); // true
+console.log(true < false); // false
+console.log(true == false); // false
+console.log(true != false); // true
+
+console.log(2 > "1"); // true
+console.log(2 < "1"); // false
+console.log(2 == "2"); // true
+console.log(2 != "2"); // false
+
+console.log(0 == false); // true
+console.log(0 === false); // false
+console.log(0 != false); // false
+console.log(0 !== false); // true
+
+console.log(null == undefined); // true
+console.log(null === undefined); // false
+console.log(null != undefined); // false
+console.log(null !== undefined); // true
+
+console.log(NaN == NaN); // false
+console.log(NaN === NaN); // false
+console.log(NaN != NaN); // true
+console.log(NaN !== NaN); // true
+
+console.log(null > 0); // false
+console.log(null >= 0); // true
+console.log(null < 0); // false
+console.log(null <= 0); // true
+console.log(null == 0); // false
+
+console.log(undefined > 0); // false
+console.log(undefined >= 0); // false
+console.log(undefined < 0); // false
+console.log(undefined <= 0); // false
+console.log(undefined == 0); // false
+
+
---
+++ basics/conversionOperation.js
@@ -0,0 +1,88 @@
+// let score = "100.50abc";
+// console.log(score);
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+// let valueInNumber = Number(score)
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+
+// //"33" => 33
+// //"33abc" => NaN
+// //"abc33" => NaN
+// //"100.50abc" => NaN
+
+// let isLoggedIn = "Mrinmoy";
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// //1 => true
+// //0 => false
+// //"Mrinmoy" => true
+// //"" => false
+// //null => false
+// //undefined => false
+
+// let someNumber = 33;
+// let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+//// ********************** Operations ************************
+
+// let value =3
+// let negValue = -value
+// console.log(value);
+// console.log(negValue);
+
+// console.log(3 + 3); //6
+// console.log(3 - 3);
+// console.log(3 * 3);
+// console.log(3 / 3);
+// console.log(3 % 2); //modulo operator gives the remainder
+// console.log(3 ** 2); //exponentiation operator gives the power
+
+// let num = 5;
+// num = num + 1; //6
+// num += 1; //7
+// num++; //8
+// console.log(num);
+
+// let num2 = 5;
+// num2 = num2 - 1; //4
+// num2 -= 1; //3
+// num2--; //2
+// console.log(num2);
+
+// let str1 = "hello";
+// let str2 = "Mrinmoy";
+// let str3 = str1 + " " + str2; //concatenation
+// console.log(str3);
+
+// console.log("1" + 2); // "1" + 2 => "12"
+// console.log(1 + "2"); // 1 + "2" => "12"
+// console.log("1" + 2 + 2); // "1" + 2 + 2 => "12" + 2 => "122"
+// console.log(1 + 2 + "2"); // 1 + 2 + "2" => 3 + "2" => "32"
+// console.log((3+4) *5 % 3); // 7 * 5 % 3 => 35 % 3 => 2  
+
+
+// console.log(+true);
+// console.log(3+true);
+
+let num1, num2 , num3;
+num1 = num2 = num3 = 2+2;
+
+let gameCounter = 100;
+++gameCounter; //101
+console.log(gameCounter); //101
+
+
+
+
+
+
+
+
