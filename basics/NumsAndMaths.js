c5e992fb673013d7c19d3503eac77a27b6225858
MrinmoyPatra-Pravaah <mrinmoy@pravaahc.com>
2026-04-28 08:57:38 +0000

    Maths and Nums operations

---
+++ basics/NumsAndMaths.js
@@ -0,0 +1,51 @@
+// const score = 100;
+// const balance = new Number(100);
+
+// console.log(score);
+// console.log(typeof score);
+
+// console.log(balance);
+// console.log(typeof balance);
+
+// // 100 => 100
+// // "100" => 100
+// // "100abc" => NaN
+// // "abc100" => NaN
+// // "100.50abc" => NaN
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+// const otherNumber = 314.159;
+// console.log(otherNumber.toFixed(2)); //3.14
+// console.log(otherNumber.toFixed(4)); //3.1416
+// console.log(otherNumber.toPrecision(3));
+
+// const hundreds = 10000000;
+// console.log(hundreds.toLocaleString());//10,000,000
+// console.log(hundreds.toLocaleString('en-IN'));//1,00,00,000
+
+//// Math object in JavaScript
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.floor(4.9));
+console.log(Math.min(4,6,3,2,6,7));
+console.log(Math.max(4,6,3,2,6,7));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+
+
+const min = 10
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min)); //10 to 20
+
+
+
+
+
+
+
+
