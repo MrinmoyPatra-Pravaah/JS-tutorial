1fd4adb5b0f2ddc9d0f880abbb4a05b728406b07
MrinmoyPatra-Pravaah <mrinmoy@pravaahc.com>
2026-04-28 05:43:56 +0000

    setup and variables

---
+++ basics/variables.js
@@ -0,0 +1,19 @@
+const acId = 12345
+let acEmail = 'abc@gmail.com'
+var acPass = '12345'
+acCity = 'kolkata'
+
+console.log(acId);
+console.log(acEmail);
+acEmail = "xyz@gmail.com"
+console.log(acEmail);
+
+console.table([acId,acEmail,acPass,acCity])
+
+
+/*
+prefer not to use var  
+because of issue in block scope and functional scope
+*/
+
+
